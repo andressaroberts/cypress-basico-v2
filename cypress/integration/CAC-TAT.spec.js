ca97767db262d30931bb91930c4e2916f8265700
Andressa <andressaroberts@gmail.com>
2022-11-21 19:02:18 +0000

    feat: using request

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -200,4 +200,15 @@ describe("Central de Atendimento ao Cliente TAT", () => {
       .invoke("val", longText)
       .should("have.value", longText);
   });
+
+  it("faz uma requisição http", () => {
+    cy.request(
+      "https://cac-tat.s3.eu-central-1.amazonaws.com/index.html"
+    ).should(function (response) {
+      const { status, statusText, body } = response;
+      expect(status).to.equal(200);
+      expect(statusText).to.equal("OK");
+      expect(body).to.include("CAC TAT");
+    });
+  });
 });
